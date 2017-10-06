d177f5801c160980ec91a7523c5a1584bb51b01b
sarafkaier <sarah.fkaier@lepoles.com>
2017-10-06 11:19:31 +0200

    4e commit
    4e commit

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Admin : prénom nom</title>
+    </head>
+    <body>
+        <h1>Admin</h1>
+    </body>
+</html>
